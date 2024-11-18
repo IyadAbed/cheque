e659fac339461fb01aa73d0d583eacc62718d5c7
IyadAbed <iyadrabed@gmail.com>
2024-11-19 01:21:19 +0300

    logOut

--- src/app/layout/topbar/topbar.component.ts
+++ src/app/layout/topbar/topbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { LayoutService } from '../service/layout.service';
 import { MenuItem } from 'primeng/api';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-topbar',
@@ -9,11 +10,16 @@ import { MenuItem } from 'primeng/api';
 export class TopbarComponent {
   items!: MenuItem[];
 
+  logOut() {
+    localStorage.removeItem('user');
+    this.router.navigate(['/auth']);
+  }
+
   @ViewChild('menubutton') menuButton!: ElementRef;
 
   @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
 
   @ViewChild('topbarmenu') menu!: ElementRef;
 
-  constructor(public layoutService: LayoutService) {}
+  constructor(public layoutService: LayoutService, private router: Router) {}
 }
